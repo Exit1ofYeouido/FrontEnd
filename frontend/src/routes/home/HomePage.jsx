b96f315ca196ff130c4598e13e8a7c37b242fd68
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-08-22 16:46:45 +0900

    ✨feat: 메인페이지 출력/#3

--- frontend/src/routes/home/HomePage.jsx
+++ frontend/src/routes/home/HomePage.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Reward from "./reward/Reward";
+import Brand from "./brand/Brand";
+import Asset from "./asset/Asset";
+import UserInfo from "./userinfo/UserInfo";
+import "./HomePage.css";
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <div>
+            <div className="home-wrapper">
+                <div className="logo"></div>
+                <Brand />
+                <Asset />
+                <UserInfo />
+                <Reward />
+            </div>
+            <Navbar />
+        </div>
+    );
 }
