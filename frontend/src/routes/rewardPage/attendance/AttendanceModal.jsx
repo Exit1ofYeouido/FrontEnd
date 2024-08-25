0017ffc4ef49095d74e778c8679247e4458ed7d2
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-08-26 08:51:17 +0900

    ⚡update: 모달 요소 수정

--- frontend/src/routes/rewardPage/attendance/AttendanceModal.jsx
+++ frontend/src/routes/rewardPage/attendance/AttendanceModal.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import styles from "./AttendanceModal.module.css"; // 모달 스타일 추가
+import styles from "./AttendanceModal.module.css";
 
 export default function AttendanceModal({ show, onClose, children }) {
-    if (!show) return null; // 모달이 보이지 않을 때 null 반환
+    if (!show) return null;
 
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
                 {children}
-                <button onClick={onClose} className={styles.closeButton}>
-                    닫기
-                </button>
+                <div className={styles.buttonGroup}>
+                    <button onClick={onClose} className={styles.companyButton}>
+                        기업 보기
+                    </button>
+                    <button onClick={onClose} className={styles.closeButton}>
+                        닫기
+                    </button>
+                </div>
             </div>
         </div>
     );
