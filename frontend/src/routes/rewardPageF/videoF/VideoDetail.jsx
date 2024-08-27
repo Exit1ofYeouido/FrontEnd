4f38039816dc23ce23cbf7eff4edf5aa5584e90c
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-08-27 23:27:01 +0900

    ⚡update: 뒤로가기, 볼륨 버튼 및 기업 소개글 배치

--- frontend/src/routes/rewardPageF/videoF/VideoDetail.jsx
+++ frontend/src/routes/rewardPageF/videoF/VideoDetail.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import YouTube from "react-youtube";
 import styles from "./VideoDetail.module.css";
 import { IoIosArrowBack } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Navbar from "~components/Navbar";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+import Slider from "@mui/material/Slider";
 
 export default function VideoDetail() {
     const navigate = useNavigate();
-    const videoId = "Xd8zr5exbKw";
-
+    const location = useLocation();
+    const videoId = "mZsAfmwldas";
+    const [player, setPlayer] = useState(null);
+    const [isMuted, setIsMuted] = useState(false);
+    const [volume, setVolume] = useState(10);
+    const [isSliderVisible, setIsSliderVisible] = useState(false);
+    const { video } = location.state || {};
     const videoOptions = {
         playerVars: {
             autoplay: 1,
             rel: 0,
             modestbranding: 1,
             controls: 0,
-            // loop: 1,
-            // playlist: videoId,
+            wmode: "opaque",            
         },
+    };
+
+    const handleVideoReady = (event) => {
+        const ytPlayer = event.target;
+        ytPlayer.setVolume(volume);
+        setPlayer(ytPlayer);
     };
 
     const handleVideoEnd = (e) => {
@@ -28,24 +40,97 @@ export default function VideoDetail() {
         navigate("/reward/video");
     };
 
+    const toggleMute = () => {
+        if (isMuted) {
+            player.unMute();
+            setIsMuted(false);
+        } else {
+            player.mute();
+            setIsMuted(true);
+        }
+    };
+
+    const handleVolumeChange = (event, newValue) => {
+        setVolume(newValue);
+        player.setVolume(newValue);
+        if (newValue === 0) {
+            setIsMuted(true);
+        } else {
+            setIsMuted(false);
+        }
+    };
+
+    const handleMouseEnter = () => {
+        setIsSliderVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsSliderVisible(false);
+    };
+
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.topBar}>
-                    <div className={styles.arrow} onClick={handleBack}>
-                        <IoIosArrowBack />
-                    </div>
-                </div>
                 <div className={styles.videoWrapper}>
                     <YouTube
                         videoId={videoId}
                         opts={videoOptions}
+                        onReady={handleVideoReady}
                         onEnd={handleVideoEnd}
                         className={styles.videoPlayer}
                     />
+                    <div className={styles.volumeControl}>
+                        <div className={styles.arrow} onClick={handleBack}>
+                            <IoIosArrowBack />
+                        </div>
+                        <div
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            className={styles.volumeContainer}
+                        >
+                            <button
+                                onClick={toggleMute}
+                                className={styles.volumeButton}
+                            >
+                                {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
+                            </button>
+                            <div className={styles.sliderBack}>
+                                {isSliderVisible && (
+                                    <Slider
+                                        value={volume}
+                                        onChange={handleVolumeChange}
+                                        aria-labelledby="continuous-slider"
+                                        min={0}
+                                        max={100}
+                                        className={styles.volumeSlider}
+                                        sx={{
+                                            width: 100,
+                                            color: "white",
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.videoInfo}>
+                        <div className={styles.company}>
+                            <img
+                                src={`https://stock-craft.s3.ap-northeast-2.amazonaws.com/logos/${encodeURIComponent(
+                                    video.name
+                                )}.svg`}
+                                alt={`${video.name} logo`}
+                                className={styles.logo}
+                            />
+                            <div className={styles.name}>{video?.name}</div>
+                        </div>
+
+                        <div className={styles.videoName}>
+                            {video?.videoName}
+                        </div>
+                    </div>
                 </div>
             </div>
-            {/* <Navbar/> */}
+            <Navbar />
         </>
     );
 }
