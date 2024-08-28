c333badb14b262a503637e87a7ba24d85758273b
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-08-28 15:53:07 +0900

    🚚rename: 폴더 변경

---
+++ frontend/src/lib/apis/rewardAPI/videoListApi.jsx
@@ -0,0 +1,13 @@
+// // import instance from "../basis";
+// import axios from "axios";
+
+// export async function videoList() {
+//     try {
+//         const response = await axios.get("http://43.203.224.71:8083/ad/info")
+//         console.log(response);
+//         return response;
+//     } catch (error) {
+//         console.log(error);
+//         return error.response;
+//     }
+// }
