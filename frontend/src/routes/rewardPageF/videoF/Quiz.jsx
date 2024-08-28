0c80c5fe281186820ba88a020bfd20fbc7b60edd
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-08-28 16:32:36 +0900

    ✨feat: 퀴즈 추가 및 api 연동

--- frontend/src/routes/rewardPageF/videoF/Quiz.jsx
+++ frontend/src/routes/rewardPageF/videoF/Quiz.jsx
@@ -1,27 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Quiz.module.css";
 import { motion } from "framer-motion";
 import { IoMdClose } from "react-icons/io";
+import axios from "axios";
+import Modal from "./QuizModal";
 
-export default function Quiz({ onClose }) {
+export default function Quiz({ onClose, mediaId, enterpriseName }) {
+    const [answer, setAnswer] = useState(0);
+    const [question, setQuestion] = useState("");
+    const [quizSectionList, setQuizSectionList] = useState([]);
+    const [amount, setAmount] = useState(0.000000);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            const response = await axios.get(`/api/ad/${mediaId}/quiz`);
+            console.log(mediaId);
+            if (response && response.data) {
+                setAnswer(response.data.answer);
+                setQuestion(response.data.question);
+                setQuizSectionList(response.data.quizSectionList);
+            } else {
+                console.error("Fail");
+            }
+        };
+        fetchVideos();
+    }, []);
+
+    const getStock = async () => {
+        const response = await axios.post(`/api/ad/${mediaId}/quiz`, {
+            enterpriseName: enterpriseName,
+        });
+
+        if (response && response.data) {
+            setAmount(response.data.amount);
+            setShowModal(true);
+        } else {
+            console.error("Fail");
+        }
+    };
+
+    const handleOptionClick = (selectedOptionIndex) => {
+        if (selectedOptionIndex === answer) {
+            getStock();
+        } else {
+            setShowModal(false);
+        }
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
     return (
-        <motion.div
-            initial={{ y: "0%", opacity: 1 }}
-            animate={{ y: "0%", opacity: 1 }}
-            exit={{ y: "100%", opacity: 0 }}
-            transition={{ duration: 1, ease: "easeInOut" }}
-            className={styles.container}
-        >
-            <div className={styles.closeButton} onClick={onClose}>
-                <IoMdClose />
-            </div>
-            <div className={styles.question}>안녕하세요</div>
-            <div className={styles.selection}>
-                <div className={styles.select}>1</div>
-                <div className={styles.select}>2</div>
-                <div className={styles.select}>3</div>
-                <div className={styles.select}>4</div>
-            </div>
-        </motion.div>
+        <>
+            <motion.div
+                initial={{ y: "0%", opacity: 1 }}
+                animate={{ y: "0%", opacity: 1 }}
+                exit={{ y: "100%", opacity: 0 }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                className={styles.container}
+            >
+                <div className={styles.closeButton} onClick={onClose}>
+                    <IoMdClose />
+                </div>
+                <div className={styles.question}>{question}</div>
+
+                <div className={styles.selection}>
+                    {quizSectionList.map((option, index) => (
+                        <div
+                            key={index + 1}
+                            className={styles.select}
+                            onClick={() => handleOptionClick(index + 1)}
+                        >
+                            {option}
+                        </div>
+                    ))}
+                </div>
+            </motion.div>
+            {showModal && (
+                <Modal
+                    onClose={closeModal}
+                    enterpriseName={enterpriseName}
+                    amount={amount}
+                />
+            )}
+        </>
     );
 }
