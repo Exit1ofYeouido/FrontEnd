6f5e2c7a52b4df14ea41487b6d25bbcd3bd1a6ea
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-09-01 14:33:15 +0900

    ⚡update: 애니메이션 추가

--- frontend/src/routes/myPageF/MyPage.jsx
+++ frontend/src/routes/myPageF/MyPage.jsx
@@ -38,119 +38,135 @@ export default function MyPage() {
     };
 
     return (
-        <div className={styles.container}>
-            <div className={styles.title}>마이페이지</div>
+        <motion.div
+            key="signin-form"
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+            <div className={styles.container}>
+                <div className={styles.title}>마이페이지</div>
 
-            {/* 포인트부분이랑께 */}
-            <div className={styles.info}>
-                <div className={styles.top}>
-                    <div className={styles.logoAccount}>
-                        <img
-                            src={accountLogo}
-                            alt="Account Logo"
-                            className={styles.accountLogo}
-                        />
-                        <div className={styles.accountId}>{data.accountId}</div>
-                    </div>
-                    <IoIosArrowForward className={styles.arrowWrapper} />
-                </div>
-                <div className={styles.pointAsset}>
-                    <img
-                        src={pointLogo}
-                        alt="point Logo"
-                        className={styles.pointLogo}
-                    />
-                    <div className={styles.pointText}>{data.totalPoint}점</div>
-                </div>
-                <div className={styles.bottom}>
-                    <button className={styles.withdrawButton}>출금하기</button>
-                </div>
-            </div>
-
-            {/* 주식 부분이랑께 */}
-            <div className={styles.stock}>
-                <div className={styles.stockTop}>
-                    <div className={styles.myStockText}>내 주식</div>
-                    <div className={styles.rate}>
-                        <div className={styles.rate1}>{data.allCost}원</div>
-                        <div className={styles.rate2}>
-                            <span className={styles.won}>
-                                {calculateProfitOrLoss(
-                                    data.earningRate,
-                                    data.allCost
-                                )}
-                                원
-                            </span>
-                            <span className={styles.percent}>
-                                ({data.earningRate}%)
-                            </span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={styles.earningRatesList}>
-                    {data.earningRates.map((rate, index) => (
-                        <div key={index} className={styles.earningRateItem}>
+                {/* 포인트부분이랑께 */}
+                <div className={styles.info}>
+                    <div className={styles.top}>
+                        <div className={styles.logoAccount}>
                             <img
-                                src={`https://stock-craft.s3.ap-northeast-2.amazonaws.com/logos/${encodeURIComponent(
-                                    rate.enterpriseName
-                                )}.svg`}
-                                alt={`${rate.enterpriseName} logo`}
-                                className={styles.logo}
+                                src={accountLogo}
+                                alt="Account Logo"
+                                className={styles.accountLogo}
                             />
-                            <div className={styles.enterpriseName}>
-                                {rate.enterpriseName}
-                            </div>
-                            <div className={styles.rateValue}>
-                                ({rate.earningRate}%)
+                            <div className={styles.accountId}>
+                                {data.accountId}
                             </div>
                         </div>
-                    ))}
+                        <IoIosArrowForward className={styles.arrowWrapper} />
+                    </div>
+                    <div className={styles.pointAsset}>
+                        <img
+                            src={pointLogo}
+                            alt="point Logo"
+                            className={styles.pointLogo}
+                        />
+                        <div className={styles.pointText}>
+                            {data.totalPoint}점
+                        </div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <button className={styles.withdrawButton}>
+                            출금하기
+                        </button>
+                    </div>
                 </div>
 
-                <div className={styles.bottom}>
-                    <button className={styles.moreButton}>더보기</button>
-                </div>
-            </div>
+                {/* 주식 부분이랑께 */}
+                <div className={styles.stock}>
+                    <div className={styles.stockTop}>
+                        <div className={styles.myStockText}>내 주식</div>
+                        <div className={styles.rate}>
+                            <div className={styles.rate1}>{data.allCost}원</div>
+                            <div className={styles.rate2}>
+                                <span className={styles.won}>
+                                    {calculateProfitOrLoss(
+                                        data.earningRate,
+                                        data.allCost
+                                    )}
+                                    원
+                                </span>
+                                <span className={styles.percent}>
+                                    ({data.earningRate}%)
+                                </span>
+                            </div>
+                        </div>
+                    </div>
 
-            {/* 주식 부분이랑께 */}
-            <div className={styles.etc}>
-                <div className={styles.actions}>
-                    <motion.div
-                        className={styles.actionItem}
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                        onClick={() => navigate("/reward/attendance")}
-                    >
-                        <div
-                            className={`${styles.icon} ${styles.calendarIcon}`}
-                        ></div>
-                        <div className={styles.actionLabel}>출석체크</div>
-                    </motion.div>
-                    <motion.div
-                        className={styles.actionItem}
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                        onClick={() => navigate("/my/point")}
-                    >
-                        <div
-                            className={`${styles.icon} ${styles.pointIcon}`}
-                        ></div>
-                        <div className={styles.actionLabel}>포인트 내역</div>
-                    </motion.div>
-                    <motion.div
-                        className={styles.actionItem}
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                        onClick={() => navigate("/home/useway")}
-                    >
-                        <div
-                            className={`${styles.icon} ${styles.useIcon}`}
-                        ></div>
-                        <div className={styles.actionLabel}>이용방법</div>
-                    </motion.div>
+                    <div className={styles.earningRatesList}>
+                        {data.earningRates.map((rate, index) => (
+                            <div key={index} className={styles.earningRateItem}>
+                                <img
+                                    src={`https://stock-craft.s3.ap-northeast-2.amazonaws.com/logos/${encodeURIComponent(
+                                        rate.enterpriseName
+                                    )}.svg`}
+                                    alt={`${rate.enterpriseName} logo`}
+                                    className={styles.logo}
+                                />
+                                <div className={styles.enterpriseName}>
+                                    {rate.enterpriseName}
+                                </div>
+                                <div className={styles.rateValue}>
+                                    ({rate.earningRate}%)
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className={styles.bottom}>
+                        <button className={styles.moreButton}>더보기</button>
+                    </div>
+                </div>
+
+                {/* 주식 부분이랑께 */}
+                <div className={styles.etc}>
+                    <div className={styles.actions}>
+                        <motion.div
+                            className={styles.actionItem}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => navigate("/reward/attendance")}
+                        >
+                            <div
+                                className={`${styles.icon} ${styles.calendarIcon}`}
+                            ></div>
+                            <div className={styles.actionLabel}>출석체크</div>
+                        </motion.div>
+                        <motion.div
+                            className={styles.actionItem}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => navigate("/my/point")}
+                        >
+                            <div
+                                className={`${styles.icon} ${styles.pointIcon}`}
+                            ></div>
+                            <div className={styles.actionLabel}>
+                                포인트 내역
+                            </div>
+                        </motion.div>
+                        <motion.div
+                            className={styles.actionItem}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => navigate("/home/useway")}
+                        >
+                            <div
+                                className={`${styles.icon} ${styles.useIcon}`}
+                            ></div>
+                            <div className={styles.actionLabel}>이용방법</div>
+                        </motion.div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
