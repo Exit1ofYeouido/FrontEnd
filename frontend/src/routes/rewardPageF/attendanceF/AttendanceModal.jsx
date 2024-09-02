f15df4d702930f22b646ad687928b2b7e1948bc6
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-09-02 11:47:57 +0900

    ⚡update: 출석체크 모달 수정

--- frontend/src/routes/rewardPageF/attendanceF/AttendanceModal.jsx
+++ frontend/src/routes/rewardPageF/attendanceF/AttendanceModal.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import styles from "./AttendanceModal.module.css";
 
-export default function AttendanceModal({ show, onClose, children }) {
-    if (!show) return null;
-
+export default function AttendanceModal({
+    onClose,
+    company,
+    amount,
+    goCompany,
+}) {
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
-                {children}
-                <div className={styles.buttonGroup}>
-                    <button onClick={onClose} className={styles.companyButton}>
+                <div className={styles.company}>
+                    <img
+                        src={`https://stock-craft.s3.ap-northeast-2.amazonaws.com/logos/${encodeURIComponent(
+                            company
+                        )}.svg`}
+                        alt={`${company} logo`}
+                        className={styles.logo}
+                    />
+                    <div className={styles.companyName}>{company}</div>
+                </div>
+                <div className={styles.stockText}>
+                    <div className={styles.stockAmount}>{amount}</div>
+                    주가 도착했어요!
+                </div>
+                <div className={styles.buttonContainer}>
+                    <button onClick={goCompany} className={styles.goCompany}>
                         기업 보기
                     </button>
-                    <button onClick={onClose} className={styles.closeButton}>
-                        닫기
+                    <button onClick={onClose} className={styles.confirm}>
+                        확인
                     </button>
                 </div>
             </div>
